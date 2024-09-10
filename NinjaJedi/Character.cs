2b65e58d2ef64334d4aa58c9811d60727cba5ecc
Senior <Senior@G-02-06>
2024-09-10 14:12:49 -0400

    Add project files.

---
+++ NinjaJedi/Character.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaJedi
+{
+    public class Character
+    {
+        private int health;
+        private int strength;
+
+        public string Faction { get; set; }
+
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value < 0)
+                {
+                    health = 0;
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
+
+        public int Strength
+        {
+            get { return strength; }
+            set
+            {
+                if (value < 0)
+                {
+                    strength = 0;
+                }
+                else
+                {
+                    strength = value;
+                }
+            }
+        }
+
+        public string SpecialSkill { get; set; }
+
+        public Character(string faction, int health, int strength, string specialSkill)
+        {
+            Faction = faction;
+            Health = health;
+            Strength = strength;
+            SpecialSkill = specialSkill;
+        }
+
+        public void DisplayCharacterInfo()
+        {
+            Console.WriteLine($"Faction: {Faction}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Strength: {Strength}");
+            Console.WriteLine($"Special Skill: {SpecialSkill}");
+        }
+
+        public void UseSpecialSkill()
+        {
+            Console.WriteLine($"Using special skill: {SpecialSkill}");
+        }
+    }
+}
---
+++ NinjaJedi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaJedi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose your faction (jedi/Ninja): ");
+            string faction = Console.ReadLine();
+
+            Console.WriteLine("Enter your health: ");
+            int health = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your strength: ");
+            int strength = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your special skill: ");
+            string specialSkill = Console.ReadLine();
+
+            Character playerCharacter = new Character(faction, health, strength, specialSkill);
+
+            Console.WriteLine("\nCharacter Created Successfully!\n");
+            playerCharacter.DisplayCharacterInfo();
+
+            Console.WriteLine("\nDo you want to use your special skill? (yes/no): ");
+            string useSpecialSkill = Console.ReadLine();
+
+            if (useSpecialSkill != null)
+            {
+                playerCharacter.UseSpecialSkill();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
